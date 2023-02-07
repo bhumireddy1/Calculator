0b15034576699ce324a7fb4c1831221c6a5cfd77
bhumiredddy1 <sunilkumareddy62@gmail.com>
2023-02-07 22:49:25 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const display = document.querySelector("#display");
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    if (btn.id ==="=") {
+      display.value = eval(display.value);
+    } else if (btn.id === "AC") {
+      display.value = "";
+    } else if (btn.id == "DE") {
+      display.value = display.value.slice(0,-1);
+    } else{
+      display.value += btn.id;
+    }
+  });
+});
